9932662f855445adb21111379d6e685c5c294007
Michael Kaplan <michael.n.kaplan@gmail.com>
2023-06-29 14:17:00 -0400

    removed early exit so it trains fully.  create a new save directory for generated image to see progression over time

--- machine_learning/images/mnist_gan.py
+++ machine_learning/images/mnist_gan.py
@@ -8,6 +8,13 @@ https://www.tensorflow.org/guide/keras/writing_a_training_loop_from_scratch
 @author: -
 
 General Adversarial Network for MNist using a custom training loop
+
+Essentially, we simultaneously train two networks.
+  Discriminator - detect real from fake images
+  Generator     - generate fake images
+  
+Training Loop 
+  -   
 """
 
 import tensorflow as tf
@@ -20,6 +27,8 @@ discriminator = keras.Sequential(
         keras.Input(shape=(28, 28, 1)),
         layers.Conv2D(64, (3, 3), strides=(2, 2), padding="same"),
         layers.LeakyReLU(alpha=0.2),
+#        layers.Conv2D(64, (3, 3), padding="same"),
+#        layers.LeakyReLU(alpha=0.2),
         layers.Conv2D(128, (3, 3), strides=(2, 2), padding="same"),
         layers.LeakyReLU(alpha=0.2),
         layers.GlobalMaxPooling2D(),
@@ -40,12 +49,15 @@ generator = keras.Sequential(
         layers.Reshape((7, 7, 128)),
         layers.Conv2DTranspose(128, (4, 4), strides=(2, 2), padding="same"),
         layers.LeakyReLU(alpha=0.2),
+        #layers.Conv2D(128, (4, 4), padding="same"),
+        #layers.LeakyReLU(alpha=0.2),
         layers.Conv2DTranspose(128, (4, 4), strides=(2, 2), padding="same"),
         layers.LeakyReLU(alpha=0.2),
         layers.Conv2D(1, (7, 7), padding="same", activation="sigmoid"),
     ],
     name="generator",
 )
+generator.summary()
 
 # Instantiate one optimizer for the discriminator and another for the generator.
 d_optimizer = keras.optimizers.Adam(learning_rate=0.0003)
@@ -101,9 +113,15 @@ all_digits = np.concatenate([x_train, x_test])
 all_digits = all_digits.astype("float32") / 255.0
 all_digits = np.reshape(all_digits, (-1, 28, 28, 1))
 dataset = tf.data.Dataset.from_tensor_slices(all_digits)
+# dataset should be 1094 elements.  70,000 images / 64 batch
 dataset = dataset.shuffle(buffer_size=1024).batch(batch_size)
+print(f"dataset length: {len(list(dataset.as_numpy_iterator()))}")
 
-epochs = 30  # In practice you need at least 20 epochs to generate nice digits.
+# 1000 epochs was probably too many.  ~200-300 seemed to level out
+# But it is starting to look good
+# NOTE: this was because we didnt' remove the step > 10 break, so each
+# epoch was being VERY truncated
+epochs = 20  # In practice you need at least 20 epochs to generate nice digits.
 save_dir = "./"
 
 for epoch in range(epochs):
@@ -125,5 +143,18 @@ for epoch in range(epochs):
 
         # To limit execution time we stop after 10 steps.
         # Remove the lines below to actually train the model!
-        if step > 10:
-            break
+        # if step > 10:
+        #     break
+        
+# Generate samples image to view
+# Sample random points in the latent space
+random_latent_vectors = tf.random.normal(shape=(batch_size, latent_dim))
+# Decode them to fake images - they look pretty terrible actually
+generated_images = generator(random_latent_vectors)        
+img_tensor = generated_images[9]
+img_numpy = img_tensor.numpy()
+# img = np.reshape(img_numpy, (28,28))
+
+# from PIL import Image
+import matplotlib.pyplot as plt
+plt.imshow(img_numpy)
